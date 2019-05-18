8720c4b129354a0095b362e0efd16c0772c347de
ShrutikaSingh <shrutika051220@gmail.com>
2019-05-18 17:16:42 +0530

    on a new mission

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';//how did this comeß
+
+class App extends Component {
+  render(){
+  return(
+    <React.Fragment>
+      This is Jack My Motor!
+    </React.Fragment>
+  )
+ }
+}
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import ReactComponents from react
+
+class home extends ReactComponents
+{
+  render()
+  return (
+<div> hi
+</div>
+
+
+);
+
+
+
+
+
+}
