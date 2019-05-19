10617e74cc93cab738b27471d77282a040b1c70e
ShrutikaSingh <shrutika051220@gmail.com>
2019-05-19 22:49:50 +0530

    added server

---
+++ Server/index.js
@@ -0,0 +1,9 @@
+const express=require(express);
+const app=express();
+
+app.get('/',res,req)=>
+{
+  res.send("started making server");
+}
+
+app.listen(3000,cosole.log(`lsiting to port ${port}``));
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 
+
 export const ButtonContainer = styled.button`
 text-tranform:capitalise;
 font-size:1.5rem;
