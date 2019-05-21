29033d6ff8fecaf9c610d596c90c566a4487c218
ShrutikaSingh <shrutika051220@gmail.com>
2019-05-21 15:46:18 +0530

    create user model

---
+++ Server/src/models/user.js
@@ -0,0 +1,23 @@
+const mongoose=require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+    },
+    password:{
+        type:String,
+        reuired:true,
+    },
+    created:{
+        type:Date,
+        default:Date.now,
+    },
+    service:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Service',
+    }], 
+});
+
+module.exports=mongoose.model('User',userSchema);
