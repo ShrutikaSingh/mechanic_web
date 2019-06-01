a7eed85cf2e3f8e684a3e2394dc3dc7bbe449a17
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-01 17:12:57 +0530

    update service.js model

--- Server/src/models/services.js
+++ Server/src/models/services.js
@@ -10,20 +10,24 @@ const serviceSchema=new mongoose.Schema({
         required:true,
     },
     model_year:{
-        type:String,
+        type:Date,
     },
     appointment:
     {
         type:Date,
+        default: Date.now,
     },
     address:{
         type:String,
+        required:true,
     },
     s_type:{
         type:String,
+        required:true,
     },
     summary:{
         type:String,
+        max:100,
     },
     purchased:[{
         type:mongoose.Schema.Types.ObjectId,
