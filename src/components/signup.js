36cbe98f133e97c1257b563abf73c1a8ef312781
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-01 17:16:54 +0530

    create signup.js for signup button in navbar

---
+++ src/components/signup.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+class Signup extends Component{
+    render(){
+        return(<div>Signup Component made on 1st June check it put if any error</div>)
+    }
+}
+
+export default Signup;
