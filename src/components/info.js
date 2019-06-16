d7f51a1e2d3538c163604443684077b14e061ad5
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-16 18:08:16 +0530

    update info of main page

---
+++ src/components/info.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class Info extends Component{
+    render(){
+        return(
+            <div>
+                <h5> We make it easy for everyone to get repaired your vehicle. </h5>
+            </div>
+        )
+    }
+}
+
+export default Info;
