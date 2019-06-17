eff26f3ef9173a4c9f5128b4bb158e2be47373c7
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-17 18:50:45 +0530

    update carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -2,28 +2,22 @@ import React,{Component} from 'react';
 import ReactDom from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from 'react-responsive-carousel';
-import {Row,Col} from 'react-bootstrap';
-import ligo from '../logo.jpg';
-import hi from '../logo.png';
-import earth from '../img/earth.png';
+import {Row,Col, Container} from 'react-bootstrap';
+
+import earth from '../img/main1.jpg';
 class CarouselMain extends Component{
     render(){
         return(
-            <React.Fragment>
-            <Carousel>
-                <div>
-                    <img src={ligo} alt="services" className="navbar-brand" height="800px"></img>    
+                <div class="main_img">
+                <Container>
+                    <Row>
+                        <Col lg="12" xs='12' sm='6' md='8'>
+                                 <img src={earth} alt="services" width="100%"></img>  
+                         </Col>
+                    </Row>
+                </Container> 
                 </div>
-                <div>
-                    <img src={ hi}/>
-                    <p className="second">Second slide</p>
-                </div>
-                <div>
-                    <img src={earth}/>
-                    <p className="third">Third slide</p>
-                </div>
-            </Carousel>
-            </React.Fragment>
+           
         );
     }
 }
