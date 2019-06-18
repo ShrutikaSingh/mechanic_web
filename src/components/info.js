9b8c80e6e84f70abbcc852cdbaeb79998cb77310
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-18 15:48:31 +0530

    add info bar column

--- src/components/info.js
+++ src/components/info.js
@@ -1,13 +1,31 @@
 import React,{Component} from 'react';
-
+import {Row,Col,Container} from 'reactstrap';
 class Info extends Component{
     render(){
         return(
-            <div>
-                <h5> We make it easy for everyone to get repaired your vehicle. </h5>
-            </div>
+            <React.Fragment>
+                <Container>
+                    <Row>
+                      <Col>
+                        <Data/>    
+
+                      </Col>
+                    </Row>
+                </Container>
+            </React.Fragment>
         )
     }
 }
+
+class Data extends Component{
+    render(){
+        return(
+            <img alt="Convenience" class="icon" src="//d310p4tz9tc9pz.cloudfront.net/assets/main/icon-easy-47c13d6dcacaa69bea11f7f20bfeeb55.svg"></img>
+        )
+    }
+}
+
+
+
 
 export default Info;
