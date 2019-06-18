48a82e2aa86e9115b8e5e16c07993f2641401d16
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-18 17:13:00 +0530

    update Server/index.js

--- Server/index.js
+++ Server/index.js
@@ -7,9 +7,6 @@ const app=express();
 //const port=8976;
 const port=process.env.PORT;
 
-
-
-
 app.use(bodyParser.urlencoded({extended:true}))
 
 const db=require('./src/models/index.js');
@@ -17,10 +14,6 @@ const db=require('./src/models/index.js');
 
 const db12=require('./src/models/index');
 var promise1=db12.connectDb();
-
-
-
-
 
 app.get('/',(req,res)=>res.send ('server is ready send the data'));
 //listing to form of index.html
