ef98c4373cd8037e63bd12618b554fd900b24a56
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-18 14:43:55 +0530

    use fluid in container

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -2,20 +2,20 @@ import React,{Component} from 'react';
 import ReactDom from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from 'react-responsive-carousel';
-import {Row,Col, Container} from 'react-bootstrap';
+import {Row,Col, Container} from 'reactstrap';
 
 import earth from '../img/main1.jpg';
 class CarouselMain extends Component{
     render(){
         return(
                 <div class="main_img">
-                
+                <Container fluid>
                     <Row>
                         <Col lg="12" xs='12' sm='12' md='12'>
                                  <img src={earth} alt="services" width='100%'></img>  
                          </Col>
                     </Row>
-               
+                </Container>
                 </div>
            
         );
