9c3c1bbd12d81d744706362aff6f7d7c768273af
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-18 16:55:57 +0530

    update info.js

--- src/components/info.js
+++ src/components/info.js
@@ -7,8 +7,13 @@ class Info extends Component{
                 <Container>
                     <Row>
                       <Col>
-                        <Data/>    
-
+                        <Data body="Get a quote and book a service online 24/7. Our mechanics will come to your home or office, even on evenings and weekends." img_link="//d310p4tz9tc9pz.cloudfront.net/assets/main/icon-easy-47c13d6dcacaa69bea11f7f20bfeeb55.svg"/>    
+                      </Col>
+                      <Col>
+                        <Data body="Get a quote and book a service online 24/7. Our mechanics will come to your home or office, even on evenings and weekends." img_link="//d310p4tz9tc9pz.cloudfront.net/assets/main/icon-shield-85a3db2fcea7ced728cdc09be095ed2e.svg"/>    
+                      </Col>
+                      <Col>
+                        <Data body="Get a quote and book a service online 24/7. Our mechanics will come to your home or office, even on evenings and weekends." img_link="//d310p4tz9tc9pz.cloudfront.net/assets/main/icon-pricing-3c7b4a919e3a18e6cdd92acbfad1b2a4.svg" />    
                       </Col>
                     </Row>
                 </Container>
@@ -20,7 +25,12 @@ class Info extends Component{
 class Data extends Component{
     render(){
         return(
-            <img alt="Convenience" class="icon" src="//d310p4tz9tc9pz.cloudfront.net/assets/main/icon-easy-47c13d6dcacaa69bea11f7f20bfeeb55.svg"></img>
+            <React.Fragment>
+            <img alt="Convenience" class="icon" src={this.props.img_link} ></img>
+            <div class="text-center">
+             <h5>{this.props.body}</h5>
+            </div>
+            </React.Fragment>
         )
     }
 }
