4581932bc36108a443a90466d28030dd1a29593c
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-19 20:56:59 +0530

    update app wd new navbar with toggler for small devices

--- src/components/nav_bar_reactstrap.js
+++ src/components/nav_bar_reactstrap.js
@@ -1,44 +1,73 @@
 import React,{Component} from 'react';
 import {Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,NavLink,NavItem,Container} from 'reactstrap';
-
-
+import ligo from '../logo.jpg';
+import {ButtonContainer} from './Button';
 class AppNavbar extends Component{       
             constructor(props){
                  super(props);
+                 this.toggle=this.toggle.bind(this);
                  this.state={
                     isOpen: false
-                }
-                this.toggle=this.toggle.bind(this);
+                };
             }
-
-            toggle=()=>{
+            toggle(){
                 this.setState({
                     isOpen: !this.state.isOpen
                 });
             }
-        
 
-        render(){
-            return(
-            <div>
-                <Navbar  color="dark" expand="sm" className="mb-5">
-                    <Container>
-                        <NavbarBrand href="/">Jack My Motor</NavbarBrand>
-                        <NavbarToggler onClick={this.toggle}/>
-                        <Collapse isOpen={this.state.isOpen} navbar/>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/about">
-                                   check
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Container>
-                </Navbar>
-            </div>
+    render(){
+        return(
+        <div>
+            <Navbar  color="light" light expand="md">
+                
+                <NavLink to='/details'>
+                        <img src={ligo} alt="services" className="navbar-brand" height="50px"></img>
+                </NavLink> 
+                <NavbarBrand href="/">Jack My Motor</NavbarBrand>
+                <NavbarToggler onClick={this.toggle}/>
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <ul className='navbar-nav align-items-center'>
+                                <li className="nav-item ml-2"> 
+                                <NavLink to='/' className="nav-link"> Home</NavLink>
+                                 </li>
+                                <li className="nav-item ml-2">
+                                    <NavLink to='/about' className="nav-link"> About</NavLink>
+                                </li>
+                                <li className="nav-item ml-2">
+                                    <NavLink to='/service' className="nav-link"> Services</NavLink>
+                                </li>
+                                
+                                <li className="nav-item ml-2">  
+                                    <NavLink to='/login' className="nav-link">
+                                        <button>Login</button>
+                                    </NavLink> 
+                                </li>
+                                <li className="nav-item ml-2"> 
+                                    <NavLink to='/signup' className="nav-link">
+                                        <button> Sign Up</button>
+                                    </NavLink> 
+                                </li>
+                                <li className="nav-item ml-2"> 
+                                    <NavLink to='/cart'>
+                                        <ButtonContainer>
+                                            <span>
+                                            <i className='fas fa-cart-plus' />
+                                            </span> Cart
+                                        </ButtonContainer>
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
             )
         }
-    }
+}
 
 
 
