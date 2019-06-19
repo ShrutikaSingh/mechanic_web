1be487aa259ea1bade036b8708368108713d8cbd
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-19 18:12:19 +0530

    creating user following bcrypt password and comparing password rules

--- Server/src/models/user.js
+++ Server/src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-
+const bcrypt=require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -22,4 +22,21 @@ const userSchema = new mongoose.Schema({
     }], 
 });
 
+//INSTALL BCRYPT
+//>npm install bcryptjs
+userSchema.pre('save', async function(next){
+    try{
+            //if the password is no modified //if we are sending in correct password we r gonna check its correct or not
+            //and in the case if nothing is changed in the database of psswd we just send dat to next function
+        if(!this.isModified('password')){
+            return next();
+        }
+//otherwise we gonna hash the password and the 10 used below is just the strength of the hashing
+const hashed= await bcrypt.hash(this.password,10);
+this.password= hashed;
+}catch(err){
+    return next(err);
+}
+});
 module.exports=mongoose.model('User',userSchema)
+
