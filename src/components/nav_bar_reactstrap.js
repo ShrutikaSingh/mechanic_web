c962fcfd2b5ac488292e0e711afaf71317763947
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-20 15:52:08 +0530

    update navbar

--- src/components/nav_bar_reactstrap.js
+++ src/components/nav_bar_reactstrap.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import {Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,NavLink,NavItem,UncontrolledDropdown,DropdownToggle,DropdownMenu,DropdownItem} from 'reactstrap';
 import ligo from '../logo.jpg';
 import {ButtonContainer} from './Button';
+import {Link} from 'react-router-dom';
+
 class AppNavbar extends Component{       
             constructor(props){
                  super(props);
@@ -18,9 +20,7 @@ class AppNavbar extends Component{
 
     render(){
         return(
-        <div>
             <Navbar  color="light" light expand="md">
-                
                 <NavLink to='/details'>
                         <img src={ligo} alt="services" className="navbar-brand" height="50px"></img>
                 </NavLink> 
@@ -31,12 +31,12 @@ class AppNavbar extends Component{
                         <NavItem>
                             <ul className='navbar-nav align-items-center'>
                                 <li className="nav-item ml-2"> 
-                                <NavLink to='/' className="nav-link"> Home</NavLink>
+                                <Link to='/' className="nav-link"> Home</Link>
                                  </li>
                                 <li>
                                     <UncontrolledDropdown nav inNavbar>
                                         <DropdownToggle nav caret>
-                                        Services
+                                        Service Offering
                                         </DropdownToggle>
                                         <DropdownMenu>
                                         <DropdownItem>
@@ -56,17 +56,21 @@ class AppNavbar extends Component{
                                         </DropdownItem>
                                         <DropdownItem>
                                          Spares & Accessories
-                                        </DropdownItem>
-                                        <DropdownItem>
-                                            Insurance & Finance
-                                        </DropdownItem>
-                                       
+                                        </DropdownItem>         
                                         </DropdownMenu>
                                     </UncontrolledDropdown>
                                 </li>
-                                
                                 <li className="nav-item ml-2">
-                                    <NavLink to='/about' className="nav-link"> About</NavLink>
+                                    <Link to='/book-service' className="nav-link"> Book My Service</Link>
+                                </li>
+                                <li className="nav-item ml-2">
+                                    <Link to='/about' className="nav-link"> About Us</Link>
+                                </li>
+                                <li className="nav-item ml-2">
+                                    <Link to='/howitworks' className="nav-link"> How It Works</Link>
+                                </li>
+                                <li className="nav-item ml-2">
+                                    <NavLink to='/about' className="nav-link"> Fair Pricing</NavLink>
                                 </li>
                                 <li className="nav-item ml-2">
                                     <NavLink to='/service' className="nav-link"> Contact</NavLink>
@@ -93,11 +97,12 @@ class AppNavbar extends Component{
                                     </NavLink>
                                 </li>
               </ul>
+
                         </NavItem>
                     </Nav>
                 </Collapse>
             </Navbar>
-        </div>
+        
             )
         }
 }
