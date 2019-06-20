21c028ad8657ac9f9587735048d8f212fe2cc231
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-20 14:54:27 +0530

    update App with proper padding and dropdown

--- src/App.js
+++ src/App.js
@@ -14,21 +14,24 @@ import CarouselMain from './components/carousel';
 import Cards from './components/cards';
 import AppNavbar from './components/nav_bar_reactstrap';
 
+
+
+const divStyle = {
+ 
+  paddingTop: '50px',
+
+};
+
 class App extends Component {
   render(){
   return(
          <React.Fragment> 
               <AppNavbar/>
-          
               <CarouselMain/> 
-              <hr></hr>
-              <div class="col-lg-12 text-center text-space mt-10 ">
+              <div class="col-lg-12 text-center text-space mt-200" style={divStyle}>
                   <h2 class="g-header-text">Get over 500 car repair services at your home or office</h2>
               </div>
-              <hr/>
               <Info/>
-              <hr/>
-             
               <Cards/>
               <div class="col-lg-4 col-md-5 col-sm-6 col-centered"><a class="btn-orange-fill" href="/about/">GET AN INSTANT QUOTE</a></div>
         </React.Fragment>
