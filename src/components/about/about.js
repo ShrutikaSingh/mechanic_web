6844d86fc465ea64494762fe9904341c8e15afbd
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-21 13:25:40 +0530

    add form in the about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import AppNavbar from '../nav_bar_reactstrap';
+import Form from '../s_form';
 class AboutUs extends Component {
   render() {
     return (
       <React.Fragment>
         <AppNavbar/>
        About Us
+       <Form/>
      
       </React.Fragment>
     )
