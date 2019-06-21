8a6e80d50a6d23b9d8667649032311e30a12da14
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-21 14:24:50 +0530

    update routed

--- src/router/chck_routes.js
+++ src/router/chck_routes.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';//how did this comeß
 import Home from '../components/home';
 import About from '../components/about/about';
-
+import Register from '../components/register';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
@@ -25,12 +25,19 @@ function AppRouter() {
             <li>
               <Link to="/users/">Users</Link>
             </li>
+            <li>
+              <Link to="/login/">Login</Link>
+            </li>
+            <li>
+              <Link to="/register/">Register</Link>
+            </li>
           </ul>
         </nav>
 
         <Route path="/home" component={Home} />
         <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} />
+        <Route path="/register/" component={Register} />
       </div>
     </Router>
   );
