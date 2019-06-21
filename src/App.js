b8de5693a83b74b52e4320fbd53055bf8ce56f9e
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-21 13:19:18 +0530

    cleaned app page withh proper components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,89 @@
 import React,{Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';//how did this comeß
-import Info from './components/info';
-import CarouselMain from './components/carousel';
-import Cards from './components/cards';
-import Routers from './router/routes';
-import S_Form from './components/s_form';
 
+import Routes from './router/chck_routes';
 
-
-const divStyle = {
-  paddingTop: '50px',
-};
 
 class App extends Component {
   render(){
   return(
          <React.Fragment> 
-              <Routers/>
-              <CarouselMain/> 
-              <div class="col-lg-12 text-center text-space mt-200" style={divStyle}>
-                  <h2 class="g-header-text">Get over 500 car repair services at your home or office</h2>
-              </div>
-              <Info/>
-              <Cards/>
-              <S_Form/>
-              <div class="col-lg-4 col-md-5 col-sm-6 col-centered"><a class="btn-orange-fill" href="/about/">GET AN INSTANT QUOTE</a></div>
+              <Routes/>
         </React.Fragment>
   )
  }
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/*}
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/home" component={Home} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
+
+
+
+*/}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
