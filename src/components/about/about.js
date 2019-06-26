2958cc37fd1bb9f2f88003567f7fb5e06bf625bb
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-26 20:30:53 +0530

    update about us page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
 import AppNavbar from '../nav_bar_reactstrap';
-import Form from '../register';
+import { Container,Row,Col } from 'reactstrap';
+import jmm from '../../img/jmm.jpg';
+import logo from '../../img/logo.png';
+
+import Content from './col_about';
+
+const divStyle = {
+  color: 'grey',
+  paddingTop: '50px',
+  
+  paddingBottom:'5%'
+};
+
+
 class AboutUs extends Component {
   render() {
     return (
       <React.Fragment>
-        <AppNavbar/>
-       About Us
-       <Form/>
+       
+         <img src={jmm} alt="services" height='200px' width="100%"></img>       
+         
+       <Container style={divStyle}>
+       <h4 >Best mechanics from your community</h4>
+      
+       
+        <Row>
+        
+          <Col style={divStyle}>
+              <h5>
+              On average our mechanics have over ten years of experience and most are master technicians with ASE Certifications.
+              <br/>
+              <br/>
+              We extensively screen all of our mechanics with background, criminal, and reference checks.
+              <br/>
+              <br/>
+              On each mechanic’s profile page, you’ll see a full list of all their certifications, years of experience, job skills, and feedback from real customers. We constantly monitor the performance of our mechanics to make sure they’ll provide you with professional and courteous service.
+              <br/>
+              <br/>
+              At YourMechanic, 
+              you can trust the people who are working on your car. 
+              </h5>
+          </Col>
+          <Col style={divStyle}>
+              <img src={logo} alt="aboutus image" width="500px" height="200px" ></img>
+              
+          </Col>
+        </Row>
+       </Container>
+       <Content/>
      
       </React.Fragment>
     )
