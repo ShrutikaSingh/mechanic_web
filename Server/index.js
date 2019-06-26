34f97e2a875c9b56b0f30673ad46c99b32332249
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-26 20:29:30 +0530

    deleted old server

--- Server/index.js
+++ Server/index.js
@@ -1,44 +1,45 @@
-require('dotenv').config();
 
-const express=require('express');
+require('dotenv').config();//create private variables that u dont want to share on a website like github
+
+
+const express = require('express');
+//const port= 5000;   //
+const port= process.env.PORT; //it will take the PORT variable from .env
+//check out why env method not working with nodemon but working perfect with node index.js 
+const cors=require('cors');
 const bodyParser=require('body-parser');
 
+const db=require('./models');    //for mongo db connection
+const handle=require('./handlers');  //start listing at /
+
+const routes=require('./routes');
 const app=express();
-//const port=8976;
-const port=process.env.PORT;
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());    //initialise the middle wares that we installed
+app.use(bodyParser.json()); //initialise the middle wares body-parser that we installed
 
-const db=require('./src/models');
-//const handle=require('./handlers');
+app.get('/',(req,res)=>res.json ({hello :'worlds'}));//json object //start listing at /
 
-const routes=require('./src/routes');
+app.use('/api/auth',routes.auth);//sending all the route for this endpoint here and routes.auth will take care of all the auth
+//go to postman to see the users  and post {"username":"ff","password":"ydfuesfrdttyefa"} to localhost:port_number/api/auth/login in the body
+//and localhost:5000/api/auth/register to register a new user  using new {"username":"tufdyhuetrtrehi","password":"ydfuesrttyefa"} and post
 
+app.use(handle.notFound);   //not found is coming from handler/index.js
 
-app.get('/',(req,res)=>res.send ('server is ready send the data'));
-//listing to form of index.html
-app.get('/form',(req,res)=>{
-    res.sendFile('/Users/shrutika/Desktop/jack_my_motor/Server/'+'index.html')
-    });
-//displaying the output of form 
-app.post('/optionsss',(req,res)=>{
-res.send(req.body);
-console.log(req.body);
-});
-
-app.use('/api/auth',routes.auth);
+//app.use( handle.errors);    //this errorHandler errors is coming from handler/index.js//const port= process.env.PORT; //it will take the PORT variable from .env
+//getting error res.status is not a function
+//app.listen(process.env.PORT || 5000 , console.log(`server is listining to ${port}`)); //``backtick for template literals
 
 
-/*promise1.then(async()=>{
-    app.listen(port,()=>
-        console.log(`listening at ${port}`),
-    );
-});
-*/
 
-//app.use(handle.notFound);
-//app.use(handle.errors);
-app.listen(process.env.PORT || 3000 ,console.log(`server listening at port :${port}`));
+app.listen(process.env.PORT,console.log(`server is ready`));
+
+
+
+
+
+
+
 
 
 
--- Server/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router= require('express').Router;
-
-const handle=require('../handlers');
-
-router.post('/register',handle.register);
-router.post('/login',handle.login);
-
-module.exports=router;
