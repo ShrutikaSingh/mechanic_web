19546b58962ea096feca7ad4e66427dc976c2034
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-26 20:50:19 +0530

    added middlerwares and authentication complete

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -3,7 +3,18 @@ const jwt= require('jsonwebtoken');
 
 
 module.exports = (req,res,next)=>{ //req,res,next just like other middle ware use if else
-if(){}
+if(req.headers.authorization){
+    // `Bearer  dsdhsjcshjbshcsb` 
+    const token=req.headers.authorization.split(' ')[1];
+    jwt.verify(token,process.env.SECRET,(err, decoded)=>{
+        if(err){
+            next(Error('Failed to authenticate token'));
+        }else{
+            req.decoded=decoded;
+            next();
+        }
+    });
+}
 else{
     next(Error('No token provided'))
 }
